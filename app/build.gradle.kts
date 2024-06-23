0ada739ebc0aafbc58d9b4202a69a42ff5521809
Shivam Shrivastava <43011013+SS7186805@users.noreply.github.com>
2024-06-23 10:15:54 +0530

    curl library added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -111,9 +111,10 @@ dependencies {
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
 
     implementation ("commons-io:commons-io:2.7")
-
-
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation ("com.github.grapesnberries:curlloggerinterceptor:0.1")
+
 }
